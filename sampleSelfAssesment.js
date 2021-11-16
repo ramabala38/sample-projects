61a314603a052909493dc08676ed20c321c8fa88
Ramanujam <37291659+ramabala38@users.noreply.github.com>
2021-11-16 23:06:09 +0530

    Create sampleSelfAssesment.js
    Sample test conditions in array functionality

---
+++ sampleSelfAssesment.js
@@ -0,0 +1,47 @@
+1.To find the average of array dynamic :
+function(table){
+  
+  return 
+}
+input=[1,2,3,4,5,6,7,8,9,0]
+
+
+2.I need to write the body of the isOnEvenPosition (table, value) function so that it returns true 
+if the value value is contained in the table array at an even index, and false otherwise. so i wrote that code:
+
+solution:function isOnEvenPosition(table,value){
+    for(var i=0;i<table.length;i++){
+        if (i % 2 === 0 && table[i] === value) {
+            return true;
+        }
+    }
+    return false;
+}
+3.function(i,j){
+  return true if any one of i and j equals to 1 and sum of i and j more than one 
+}
+for example = i=1,j=2  true
+for example i=3,j=4   false
+
+4./**
+ * From a collection of numbers inside an array, returns the closest value to zero.
+ */
+function closestToZero(numbers) {
+    if(!numbers.length){
+        return 0;
+    }
+    
+    let closest = 0;
+    
+    for (let i = 0; i < numbers.length ; i++) {
+        if (closest === 0) {
+            closest = numbers[i];
+        } else if (numbers[i] > 0 && numbers[i] <= Math.abs(closest)) {
+            closest = numbers[i];
+        } else if (numbers[i] < 0 && - numbers[i] < Math.abs(closest)) {
+            closest = numbers[i];
+        }
+    }
+    
+    return closest;
+}
